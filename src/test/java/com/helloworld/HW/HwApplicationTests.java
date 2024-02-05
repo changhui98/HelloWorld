864d8b54a4b94b9a3b8c8a2bd79c3de420a83a71
changhui98 <net_0102@naver.com>
2024-02-05 22:26:19 +0900

    1.0 Initial setting

---
+++ src/test/java/com/helloworld/HW/HwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.HW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
