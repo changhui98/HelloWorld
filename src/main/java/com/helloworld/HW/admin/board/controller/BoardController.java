a3e851e8b231e3515ced14b4280bbedb3c879db7
changhui98 <net_0102@naver.com>
2024-02-13 17:56:24 +0900

    admin 페이지 템플릿 뷰 추가 및 연동(add,list,form)

--- src/main/java/com/helloworld/HW/admin/board/controller/BoardController.java
+++ src/main/java/com/helloworld/HW/admin/board/controller/BoardController.java
@@ -1,9 +1,24 @@
 package com.helloworld.HW.admin.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin/board")
 public class BoardController {
 
+
+    @GetMapping("/add")
+    public String add(){
+
+        return "admin/board/add";
+    }
+
+    @GetMapping("/list")
+    public String list(){
+
+        return "admin/board/list";
+    }
 
 }
