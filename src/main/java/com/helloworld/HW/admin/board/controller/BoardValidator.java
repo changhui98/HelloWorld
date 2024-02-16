00828712e05430a3437d3f11834f7f5df6a7777a
changhui98 <net_0102@naver.com>
2024-02-16 16:16:17 +0900

    게시판 등록 시 id 중복체크, 중복 발생시 메시지 출력

---
+++ src/main/java/com/helloworld/HW/admin/board/controller/BoardValidator.java
@@ -0,0 +1,37 @@
+package com.helloworld.HW.admin.board.controller;
+
+import com.helloworld.HW.admin.board.dto.RequestBoardConfig;
+import com.helloworld.HW.admin.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class BoardValidator implements Validator {
+
+    private final BoardRepository boardRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(RequestBoardConfig.class);
+    }
+
+    /**'
+     * 게시판 아이디 중복 체크
+     *
+     * @param target
+     * @param errors
+     */
+    @Override
+    public void validate(Object target, Errors errors) {
+        RequestBoardConfig form = (RequestBoardConfig) target;
+
+        String bid= form.getBid();
+        if(StringUtils.hasText(bid) && boardRepository.existsById(bid)) {
+            errors.rejectValue("bid", "Duplicated");
+        }
+    }
+}
---
+++ src/main/java/com/helloworld/HW/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package com.helloworld.HW.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setDefaultEncoding("UTF-8");
+        ms.setBasenames("messages.commons", "messages.errors", "messages.validations");
+
+        return ms;
+    }
+}
