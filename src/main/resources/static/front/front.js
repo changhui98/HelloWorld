6bd415091a4d3607c04224364fea17deff35fd85
6uiwj <oterges00@gmail.com>
2024-02-22 11:14:32 +0900

    240222 front 헤더&메인페이지 css/js/레이아웃 기본구성

---
+++ src/main/resources/static/front/front.js
@@ -0,0 +1,33 @@
+let header = document.querySelector('header'),
+    nav = document.querySelector('nav');
+
+     nav.addEventListener('mouseover', function(){
+        header.style.height = '220px';
+     });
+
+     nav.addEventListener('mouseout', function(){
+        header.style.height = '100px';
+     });
+
+/* 아래 코드는 먹지 않음.  */
+
+let mainMenuList = document.querySelectorAll('.mainmenu > li'),
+    subMenu = document.querySelectorAll('.submenu'),
+    headerHeight = header.offsetHeight,
+    subMenuHeight = 0;
+
+    for(let i = 0; i< subMenu.length; i++){
+        if(subMenu[i].offsetHeight > subMenuHeight){
+            subMenuHeight = subMenu[i].offsetHeight;
+        }
+    }
+
+    for(let j = 0; j< mainMenuList.length; j++){
+        mainMenuList[j].addEventListener('mouseover', function(){
+            header.style.height = headerHeight + subMenuHeight + 'px';
+        });
+
+        mainMenuList[j].addEventListener('mouseout', function(){
+            header.style.height = headerHeight + 'px';
+        });
+    }
